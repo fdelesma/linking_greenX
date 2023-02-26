de3a72b010928bf4abc25c3a01685a4c35a51e4d
Francisco Delesma <86665922+fdelesma@users.noreply.github.com>
2023-02-26 18:51:37 +0200

    Create initial_cache.example.cmake

---
+++ initial_cache.example.cmake
@@ -0,0 +1,21 @@
+set(CMAKE_INSTALL_PREFIX "$ENV{AIMS_HOME}" CACHE STRING "")
+
+###############
+# Basic Flags #
+###############
+set(CMAKE_Fortran_COMPILER mpiifort CACHE STRING "")
+set(CMAKE_Fortran_FLAGS "-O3 -ip -fp-model precise" CACHE STRING "")
+set(Fortran_MIN_FLAGS "-O0 -fp-model precise" CACHE STRING "")
+
+###############
+# C,C++ Flags #
+###############
+set(CMAKE_C_COMPILER icc CACHE STRING "")
+set(CMAKE_C_FLAGS "-O3 -ip -fp-model precise -std=gnu99" CACHE STRING "")
+
+###############
+set(INC_PATHS "$ENV{GX_ROOT}/install/include/modules" CACHE STRING "")
+set(LIB_PATHS "$ENV{MKLROOT}/lib/intel64" "$ENV{GX_ROOT}/install/lib" CACHE STRING "")
+set(LIBS "mkl_scalapack_lp64 mkl_blacs_intelmpi_lp64 mkl_intel_lp64 mkl_sequential mkl_core gx_minimax" CACHE STRING "")
+set(USE_GREENX ON CACHE BOOL "")
+set(USE_SCALAPACK ON CACHE BOOL "")
